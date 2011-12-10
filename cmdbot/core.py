5a715679cca308fd311f063f31eeb277bd4709f6
Bruno Bord <brunobord@gmail.com>
2011-12-10 15:45:40 +0100

    change in API: now we've just got to run()

--- cmdbot/core.py
+++ cmdbot/core.py
@@ -64,16 +64,9 @@ class Line(object):
         return '<%s: %s>' % (self.nick_from, self.message)
 
 
-class Bot(object):
-    "Main bot class"
-
-    class Brain(object):
-        pass
-
-    welcome_message = "Hi everyone."
-    exit_message = "Bye, all"
-
-    def __init__(self, config):
+class Configuration(object):
+    "Basic Configuration class. Loads a .ini file "
+    def __init__(self):
         # the only mandatory arguments
         default_vars = {
             'port': '6667',
@@ -82,6 +75,14 @@ class Bot(object):
             'realname': 'Cmd Bot',
             'admins': '',
         }
+        parser = argparse.ArgumentParser("CmdBot")
+        parser.add_argument('ini_file',
+            help='path to the ini file to extract configuration from')
+        args = parser.parse_args()
+
+        config = SafeConfigParser()
+        config.read(args.ini_file)
+
         self.host = config.get('general', 'host')
         self.chan = str(config.get('general', 'chan'))
 
@@ -90,6 +91,23 @@ class Bot(object):
         self.ident = config.get('general', 'ident', vars=default_vars)
         self.realname = config.get('general', 'realname', vars=default_vars)
         self.admins = config.get('general', 'admins', vars=default_vars).split()
+
+
+class Bot(object):
+    "Main bot class"
+
+    class Brain(object):
+        pass
+
+    welcome_message = "Hi everyone."
+    exit_message = "Bye, all"
+    # One can override this
+    config_class = Configuration
+
+    def __init__(self):
+        self.config = self.config_class()
+        # special case: admins
+        self.admins = self.config.admins
         self.brain = self.Brain()  # this brain can contain *anything* you want.
 
         self.available_functions = []
@@ -104,30 +122,30 @@ class Bot(object):
     def connect(self):
         "Connect to the server and join the chan"
         logging.info("Connection to host...")
-        self.s.connect((self.host, self.port))
-        self.s.send("NICK %s\r\n" % self.nick)
+        self.s.connect((self.config.host, self.config.port))
+        self.s.send("NICK %s\r\n" % self.config.nick)
         self.s.send("USER %s %s bla :%s\r\n" % (
-            self.ident, self.host, self.realname))
-        self.s.send("JOIN :%s\r\n" % self.chan)
+            self.config.ident, self.config.host, self.config.realname))
+        self.s.send("JOIN :%s\r\n" % self.config.chan)
         self.say(self.welcome_message)
 
     def say(self, message):
         "Say that `message` to the channel"
-        msg = 'PRIVMSG %s :%s\r\n' % (self.chan, message)
+        msg = 'PRIVMSG %s :%s\r\n' % (self.config.chan, message)
         self.s.send(msg)
 
     def parse_line(self, line):
         "Analyse the line. Return a Line object"
         message = nick_from = ''
         direct = False
-        meta, _, raw_message = line.partition(self.chan)
+        meta, _, raw_message = line.partition(self.config.chan)
         # strip strings
         raw_message = raw_message.strip()
         # extract initial nick
         meta = meta.strip()
         nick_from = meta.partition('!')[0].replace(':', '')
 
-        if raw_message.startswith(':%s' % self.nick):
+        if raw_message.startswith(':%s' % self.config.nick):
             direct = True
             _, _, message = raw_message.partition(' ')
         else:
@@ -186,7 +204,7 @@ class Bot(object):
                     raw_line = raw_line.rstrip()
                     if raw_line.startswith('PING'):
                         self._raw_ping(raw_line)
-                    elif self.chan in raw_line and 'PRIVMSG' in raw_line:
+                    elif self.config.chan in raw_line and 'PRIVMSG' in raw_line:
                         logging.debug(raw_line)
                         line = self.parse_line(raw_line)
                         self.process_line(line)
@@ -196,12 +214,5 @@ class Bot(object):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser("CmdBot")
-    parser.add_argument('ini_file',
-        help='path to the ini file to extract configuration from')
-    args = parser.parse_args()
-
-    config = SafeConfigParser()
-    config.read(args.ini_file)
-    bot = Bot(config)
+    bot = Bot()
     bot.run()
